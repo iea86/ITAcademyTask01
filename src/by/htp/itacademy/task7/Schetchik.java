37d4255f89384d5cc5c0d91881cb79c5c8d4ebd5
iea86 <40144853+iea86@users.noreply.github.com>
2020-02-16 22:51:09 +0300

    Add files via upload

---
+++ src/by/htp/itacademy/task7/Schetchik.java
@@ -0,0 +1,38 @@
+package by.htp.itacademy.task75;
+
+public class Schetchik {
+	private int start;
+	private int end;
+	private int currentState;
+
+	public Schetchik() {
+
+	}
+
+	public Schetchik(int start, int end, int currentState) {
+		this.start = start;
+		this.end = end;
+		this.currentState = currentState;
+	}
+
+	public void toIncrease() {
+		currentState++;
+
+		if (currentState > end) {
+			currentState = 0;
+		}
+	}
+
+	public void toDecrease() {
+		currentState--;
+
+		if (currentState < start) {
+			currentState = 0;
+		}
+	}
+
+	public int getCurrentState() {
+		return currentState;
+	}
+
+}
