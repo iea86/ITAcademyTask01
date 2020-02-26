ec6f7d598b0941ad9a280b71b5e017272c060d5e
iea86 <40144853+iea86@users.noreply.github.com>
2020-02-26 19:55:46 +0300

    Update Task01.java

--- src/by/htp/itacademy/task6/Task01.java
+++ src/by/htp/itacademy/task6/Task01.java
@@ -1,4 +1,4 @@
-package by.training.tasks.aliv.masmas;
+package by.htp.itacademy.task6;
 
 //1. Cоздать матрицу 3 x 4, заполнить ее числами 0 и 1 так, чтобы в одной строке была ровно одна единица, и вывести на экран.
 
